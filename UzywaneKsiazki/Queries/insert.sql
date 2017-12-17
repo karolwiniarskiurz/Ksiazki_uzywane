5192864ad0a666234b907c06d44c1a98142ffcad
Y-50\Karol <kwiniarski97@gmail.com>
2017-12-18 00:15:33 +0100

    i like commits

--- UzywaneKsiazki/Queries/insert.sql
+++ UzywaneKsiazki/Queries/insert.sql
@@ -1,1 +1,1 @@
-insert into Posts values(NEWID(), 'dasdada', 'dsadasda' , GETDATE(), 'adasda','sdadasda', 12.12, GETDATE(), 'new', '3123132131', 'asdasdsa', 'sdadas')
+insert into Posts values(NEWID(), 'dasdasda', 'dsadasda' , GETDATE(), 'adasda','sdadasda', 12.12, GETDATE(), 'new', '3123132131', 'asdasdsa', 'sdadas')
